6a02031488ec8b9193fbb45daacfc534a53ee66e
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-06-02 17:34:06 +0700

    // deklarasi array data dengan panjang index 3

--- exceptionStandart.cpp
+++ exceptionStandart.cpp
@@ -5,5 +5,14 @@ using namespace std;
 
 int main()
 {
-    cout << "Awal program" << endl; // penanda
+    cout << "Awal program" << endl; // penanda 1
+    try
+    {
+        // deklarasi array data dengan panjang index 3
+        array<int, 3> data = {1, 2, 3};
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
